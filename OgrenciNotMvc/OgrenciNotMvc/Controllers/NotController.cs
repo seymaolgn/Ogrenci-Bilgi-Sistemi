c7bf019338fff4f13208fa27b5785024b9dd4414
seymaolgn <81528821+seymaolgn@users.noreply.github.com>
2021-06-13 15:56:49 +0300

    Ögrenci Bilgi Sistemi
    Öğrenci ve okul işlemlerinin içeren Mvc Web projesidir

---
+++ OgrenciNotMvc/OgrenciNotMvc/Controllers/NotController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OgrenciNotMvc.Models.EntityFramework;
+using OgrenciNotMvc.Models;
+
+namespace OgrenciNotMvc.Controllers
+{
+    public class NotController : Controller
+    {
+        // GET: Not
+        DbMvcOkulEntities db = new DbMvcOkulEntities();
+        public ActionResult Index()
+        {
+            var SınavNot = db.TBLNOTLAR.ToList();
+            return View(SınavNot);
+        }
+        [HttpGet]
+        public ActionResult YeniSınav()
+        {
+            return View();
+        }
+        [HttpPost]    
+        public ActionResult YeniSınav(TBLNOTLAR p4)
+        {
+            db.TBLNOTLAR.Add(p4);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult NotGetir(int id)
+        {
+            var not = db.TBLNOTLAR.Find(id);
+            return View("NotGetir", not);
+        }
+        [HttpPost]
+        public ActionResult NotGetir(Class1 model,TBLNOTLAR p,int SINAV1=0, int SINAV2=0,int SINAV3=0,int PROJE=0)
+        {
+            
+            if (model.islem == "hesapla")
+            {
+                int ortalama = (SINAV1+ SINAV2 + SINAV3 + PROJE) / 4;
+                ViewBag.ort = ortalama;
+            }
+            if(model.islem =="notGuncelle")
+            {
+                var nt = db.TBLNOTLAR.Find(p.NOTID);
+                nt.SINAV1 = p.SINAV1;
+                nt.SINAV2 = p.SINAV2;
+                nt.SINAV3 = p.SINAV3;
+                nt.PROJE = p.PROJE;
+                nt.ORTALAMA = p.ORTALAMA;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ OgrenciNotMvc/OgrenciNotMvc/Controllers/OgrenciController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OgrenciNotMvc.Models.EntityFramework;
+
+namespace OgrenciNotMvc.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        // GET: Ogrenci
+        DbMvcOkulEntities db = new DbMvcOkulEntities();
+        public ActionResult Index()
+        {
+            var ogrenci = db.TBLOGRENCILER.ToList();
+            return View(ogrenci);
+        }
+        [HttpGet]
+        public ActionResult YeniOgrenci()
+        {
+
+            List<SelectListItem> kulup = (from i in db.TBLKULUPLER.ToList()
+                                          select new SelectListItem
+                                          {
+                                              Text = i.KULUPAD,
+                                              Value = i.KULUPID.ToString()
+                                          }).ToList();
+            ViewBag.klp = kulup;              
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniOgrenci(TBLOGRENCILER p3)
+        {
+            var klp = db.TBLKULUPLER.Where(x => x.KULUPID == p3.TBLKULUPLER.KULUPID).FirstOrDefault();
+            p3.TBLKULUPLER = klp;
+            db.TBLOGRENCILER.Add(p3);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+            var ogr = db.TBLOGRENCILER.Find(id);
+            db.TBLOGRENCILER.Remove(ogr);
+            db.SaveChanges(); 
+            return RedirectToAction("Index");
+        }
+        public ActionResult OgrenciGetir(int id)
+        {
+            var ogrenci = db.TBLOGRENCILER.Find(id);
+            List<SelectListItem> kulup = (from i in db.TBLKULUPLER.ToList()
+                                          select new SelectListItem
+                                          {
+                                              Text = i.KULUPAD,
+                                              Value = i.KULUPID.ToString()
+                                          }).ToList();
+            ViewBag.klp = kulup;
+            return View("OgrenciGetir", ogrenci);
+        }
+        public ActionResult guncelle(TBLOGRENCILER p)
+        {
+            var ogr = db.TBLOGRENCILER.Find(p.OGRENCIID);
+            ogr.OGRAD = p.OGRAD;
+            ogr.OGRSOYAD = p.OGRSOYAD;
+            ogr.OGRFOTO = p.OGRFOTO;
+            ogr.OGRKULUP = p.OGRKULUP;
+            ogr.OGRCINSIYET = p.OGRCINSIYET;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
